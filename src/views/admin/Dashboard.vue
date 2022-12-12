f759b50e082a3dea02c3ea45d7292e56163893a4
Miguel Ramos <mmartinrm3@gmail.com>
2022-12-12 11:00:38 -0500

    last changes

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -9,12 +9,12 @@
       </div>
     </div>
     <div class="flex flex-wrap mt-4">
-      <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+      <div class="w-full  mb-12 xl:mb-0 px-4">
         <card-page-visits />
       </div>
-      <div class="w-full xl:w-4/12 px-4">
+      <!-- <div class="w-full xl:w-4/12 px-4">
         <card-social-traffic />
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
